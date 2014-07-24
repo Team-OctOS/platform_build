dd55313b8eca078fd2a7ddcc2bc810f41cb6536a
sohkis <sohkis@gmail.com>
2014-07-24 14:44:17 -0400

    ArchiDroid Optimizations v3
    Conflicts:
    	core/combo/TARGET_linux-arm.mk
    	core/combo/arch/arm/armv7-a-neon.mk
    	core/combo/select.mk

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -9,11 +9,12 @@ ARCH_ARM_HAVE_NEON              := true
 ifeq ($(TARGET_CPU_VARIANT),$(filter $(TARGET_CPU_VARIANT),cortex-a15 krait))
 	arch_variant_cflags := -mcpu=cortex-a15 -mfpu=neon-vfpv4
 else
-ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a9)
-	arch_variant_cflags := -mcpu=cortex-a9 -mfpu=neon
+ifeq ($(strip $(TARGET_CPU_VARIANT)), cortex-a9)
+	arch_variant_cflags := -mcpu=cortex-a9
 else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a8)
-	arch_variant_cflags := -mcpu=cortex-a8 -mfpu=neon
+	arch_variant_cflags := -mcpu=cortex-a8
+	arch_variant_ldflags := -Wl,--fix-cortex-a8
 else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a7)
 	arch_variant_cflags := -mcpu=cortex-a7 -mfpu=neon-vfpv4
@@ -33,7 +34,5 @@ endif
 endif
 
 arch_variant_cflags += \
-    -mfloat-abi=softfp
-
-arch_variant_ldflags := \
-	-Wl,--fix-cortex-a8
+    -mfloat-abi=softfp \
+    -mfpu=neon
