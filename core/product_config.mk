0ef07c7b1ed65eff7555d6b3231206c6c5529773
Brint E. Kriebel <bekit@cyngn.com>
2014-10-14 12:43:28 -0700

    product_config: Set TO_BUILD priority above TARGET_BUILD_APPS
    Even if we are doing TARGET_BUILD_APPS, we may want to use a TO
    target for building.

--- core/product_config.mk
+++ core/product_config.mk
@@ -179,19 +179,20 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-ifneq ($(strip $(TARGET_BUILD_APPS)),)
-# An unbundled app build needs only the core product makefiles.
-all_product_configs := $(call get-product-makefiles,\
-    $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
+# A Team OctOS build needs only the TO product makefiles.
+ifneq ($(TO_BUILD),)
+  all_product_configs := $(shell ls vendor/to/products/to_$(TO_BUILD).mk)
 else
-  ifneq ($(TO_BUILD),)
-    all_product_configs := $(shell ls vendor/to/products/to_$(TO_BUILD).mk)
+  ifneq ($(strip $(TARGET_BUILD_APPS)),)
+  # An unbundled app build needs only the core product makefiles.
+  all_product_configs := $(call get-product-makefiles,\
+      $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif
-endif
+  endif # TARGET_BUILD_APPS
+endif # TO_BUILD
 
 ifeq ($(TO_BUILD),)
 # Find the product config makefile for the current product.
