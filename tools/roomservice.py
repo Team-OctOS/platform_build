9bd9c9c53c1f5fecaa12751fecad0d1845330ad8
hedwig34 <hedwig34@gmail.com>
2014-11-03 23:49:31 -0500

    Update roomservice for Team-OctOS
    Change-Id: I46ec98c6fa7e18cda96649ce118590e0f5f3ff83

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -36,14 +36,14 @@ except ImportError:
 # set this to the default remote to use in repo
 default_rem = "ok"
 # set this to the default revision to use (branch/tag name)
-default_rev = "WIP" #Temp branch until merged
+default_rev = "kk-4.4" #Temp branch until merged
 # set this to the remote that you use for projects from your team repos
 # example fetch="https://github.com/omnirom"
 default_team_rem = "ok"
 # this shouldn't change unless google makes changes
 local_manifest_dir = ".repo/local_manifests"
 # change this to your name on github (or equivalent hosting)
-platform_team = "Octo-Kat"
+platform_team = "Team-OctOS"
 
 
 def check_repo_exists(git_data):
