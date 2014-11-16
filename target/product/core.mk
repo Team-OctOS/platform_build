22421a0ce1c186eaa42deb8fdeb29713e1486b09
Altaf-Mahdi <altaf.mahdi@gmail.com>
2014-11-16 17:19:07 +0000

    remove some bloat
    Change-Id: Icb5452d57b81e2fe7d2a08743a24a07cd3cb1a17

--- target/product/core.mk
+++ target/product/core.mk
@@ -32,22 +32,18 @@ PRODUCT_PACKAGES += \
     DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    Email \
-    Exchange2 \
     ExternalStorageProvider \
     FusedLocation \
     InputDevices \
     KeyChain \
     Keyguard \
     LatinIME \
-    Launcher2 \
     ManagedProvisioning \
     PicoTts \
     PacProcessor \
     libpac \
     PrintSpooler \
     ProxyHandler \
-    QuickSearchBox \
     Settings \
     SharedStorageBackup \
     Telecom \
