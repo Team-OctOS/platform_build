1c14af69250d1bf2661dafa97024175518306e8b
tsubus <tsubus@ilwt.org>
2016-09-26 14:39:28 +0200

    Include common TO sepolicy
    Change-Id: Ib188cf54c7461d9b0396a4a2432f33e01bedbbcc

--- core/config.mk
+++ core/config.mk
@@ -883,4 +883,7 @@ RSCOMPAT_NO_USAGEIO_API_LEVELS := 8 9 10 11 12 13
 # Rules for QCOM targets
 include $(BUILD_SYSTEM)/qcom_target.mk
 
+# TO common sepolicy
+include $(TOPDIR)vendor/to/sepolicy/sepolicy.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk
